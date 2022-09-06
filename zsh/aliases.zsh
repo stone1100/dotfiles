d664be0f8fe27570c8b3f9764370f4e0640bcc78
stone1100 <stonejh1100@gmail.com>
2022-09-06 22:41:29 +0800

    add java config

---
+++ zsh/aliases.zsh
@@ -0,0 +1,11 @@
+alias ls='logo-ls'
+alias la='logo-ls -A'
+alias ll='logo-ls -al'
+# equivalents with Git Status on by Default
+alias lsg='logo-ls -D'
+alias lag='logo-ls -AD'
+alias llg='logo-ls -alD'
+alias vim='nvim'
+alias vi='nvim'
+# Re-run source command on .zshrc to reload current terminal session with new settings
+alias reload="source ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
