d2196e7ed223c5486e70d11fd4292a7f4536ccb6
stone1100 <stonejh1100@gmail.com>
2022-11-18 08:58:05 +0800

    add stylelint/tmux/zshrc

---
+++ nvim/lua/server/stylelint_lsp.lua
@@ -0,0 +1,13 @@
+local nvim_lsp = require("lspconfig")
+local utils = require("utils")
+
+-- override language server settings
+nvim_lsp.stylelint_lsp.setup({
+	on_attach = utils.on_attach,
+	settings = {
+		stylelintplus = {
+			autoFixOnSave = true,
+			autoFixOnFormat = true,
+		}
+	},
+})
