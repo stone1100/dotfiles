dde466fdec297b8c197326837e1fb1ffa8885737
stone1100 <stonejh1100@gmail.com>
2022-11-18 21:44:16 +0800

    add filetype for stylelinit

--- nvim/lua/server/stylelint_lsp.lua
+++ nvim/lua/server/stylelint_lsp.lua
@@ -4,6 +4,7 @@ local utils = require("utils")
 -- override language server settings
 nvim_lsp.stylelint_lsp.setup({
 	on_attach = utils.on_attach,
+	filetypes = { "css", "scss", "less" },
 	settings = {
 		stylelintplus = {
 			autoFixOnSave = true,
